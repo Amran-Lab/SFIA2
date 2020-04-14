0c216f1468056ff38039e418827b71cdab1811a5
Amran-Lab <amran3210@gmail.com>
2020-04-14 08:54:04 +0000

    tingy test

--- service_1/testing.py
+++ service_1/testing.py
@@ -5,12 +5,12 @@ import application
 import app
 import os 
 app = Flask(__name__)
-#sqlhost = os.environ['SQLHOST']
-#sqlpass = os.environ['SQLPASS']
-app.config['MYSQL_HOST'] = '35.189.122.57'
+sqlhost = os.environ['SQLHOST']
+sqlpass = os.environ['SQLPASS']
+app.config['MYSQL_HOST'] = sqlhost
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '09Ksg9yJf0M57mgq'
-app.config['MYSQL_DB']= 'DockerBase'
+app.config['MYSQL_PASSWORD'] = sqlpass
+app.config['MYSQL_DB']= 'project1'
 
 mysql = MySQL(app)
 
@@ -23,12 +23,24 @@ def test_about():
     http = urllib3.PoolManager()
     r = http.request('GET', 'http://35.246.116.170:5000/')
     assert 200 == r.status
-"""
+
 def test_login():
     http = urllib3.PoolManager()
     r = http.request('GET', 'http://35.239.238.29:5000/')
+    assert 200 == r.status
+def test_home1():
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://35.222.148.112:5000/about')
     assert 404 == r.status
-"""
+def test_about2():
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://35.246.116.170:5000/about')
+    assert 404 == r.status
+
+def test_login3():
+    http = urllib3.PoolManager()
+    r = http.request('GET', 'http://35.239.238.29:5000/about')
+    assert 404 == r.status
 def test_insert_db():
     with app.app_context():                                                      #need context for it to work
         cur = mysql.connection.cursor()
